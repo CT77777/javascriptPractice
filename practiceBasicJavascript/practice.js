2d8f8f1eae815ff17c61f2d5c6baf489baf61776
CT77777 <103264872+CT77777@users.noreply.github.com>
2023-02-21 14:40:34 +0800

    update practice.js

--- practiceBasicJavascript/practice.js
+++ practiceBasicJavascript/practice.js
@@ -6,9 +6,9 @@ const firstName = prompt("Please enter your first name.");
 const lastName = prompt("Please enter your last name.");
 
 // 4. Create a third variable and which combines an uppercase version values in the two other variables separated by a space. For example, if the first two variables contain "sally" and "forth", this third variable should contain the string value "SALLY FORTH"
-const combineName = firstName + " " + lastName;
-console.log(combineName);
+const combineName = firstName.toUpperCase() + " " + lastName.toUpperCase();
+
 // 5. Create a fourth variable to store a number. The number should be the total number of characters in the third variable.
-let number = combineName.length;
+const number = combineName.length;
 // 6. Add an alert dialog box that says "The string '[insert value of third variable here]' is X number of characters long." For example, if the third variable contained the string "SALLY FORTH" then the alert dialog should says "The string 'SALLY FORTH' is 11 characters long."
-alert(`The string "${combine}" is ${number} number of characters long.`);
+alert(`The string "${combineName}" is ${number} number of characters long.`);

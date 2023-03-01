57be0a6253ff5c014842fb7039c6ff4e2937d991
CT77777 <103264872+CT77777@users.noreply.github.com>
2023-03-01 11:53:29 +0800

    commit randomNumberChallenge.js

---
+++ JavaScript Numbers - Create a Program with Math/randomNumberChallenge.js
@@ -0,0 +1,15 @@
+// Collect input from a user
+const input = prompt("Please input a integer number.");
+console.log(typeof input);
+
+// Convert the input to a number
+const inputNumber = parseInt(input);
+console.log(typeof inputNumber);
+
+// Use Math.random() and the user's number to generate a random number
+const randomNumber = Math.floor(Math.random() * inputNumber) + 1;
+
+// Create a message displaying the random number
+document.querySelector(
+  "main"
+).innerHTML = `<h1>Random number is ${randomNumber} from 1 to ${inputNumber}</h1>`;
